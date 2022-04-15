23c9294504ba79aedaeb6b1693a7ef0b493ce956
Ha0Lee <101060048+budKoder@users.noreply.github.com>
2022-04-15 11:34:56 +0900

    Refactor#2839
    숫자계산방식

--- 2839.py
+++ 2839.py
@@ -1,21 +1,13 @@
 import sys
 n = int(sys.stdin.readline())
 
-cnt_list = [-1 for _ in range(n)]
-
-cnt_list[2] = 1
-if n >= 5:
-    cnt_list[4]=1
-
-for i in range(5,n):
-    cnt1 = cnt_list[i-3]
-    cnt2 = cnt_list[i-5]
-
-    if cnt1 != -1 and cnt2 != -1:
-        cnt_list[i] = min(cnt1, cnt2)+1
-    elif cnt1 != -1 and cnt2 == -1:
-        cnt_list[i] = cnt1 + 1
-    elif cnt1 == -1 and cnt2 != -1:
-        cnt_list[i] = cnt2 + 1
-
-print(cnt_list[-1])
+cnt = 0
+while n >= 0:
+    if n % 5 == 0:
+        cnt += (n//5)
+        print(cnt)
+        break
+    n -= 3
+    cnt += 1
+else:
+    print(-1)
