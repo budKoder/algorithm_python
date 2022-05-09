51bbd4e6cef1d51bb52fe6c86f27d958c45b60b7
Ha0Lee <101060048+budKoder@users.noreply.github.com>
2022-05-09 13:30:48 +0900

    Refacotring BOJ#10815
    using set

--- 10815.py
+++ 10815.py
@@ -1,24 +1,11 @@
 import sys
-
-
-def binSearch(arr, num):
-    low = 0
-    high = len(arr)-1
-    while low <= high:
-        mid = (low+high)//2
-        if arr[mid] == num:
-            return 1
-        elif arr[mid] > num:
-            high= mid - 1
-        elif arr[mid] < num:
-            low = mid + 1
-    return 0
-
-
 n = int(sys.stdin.readline())
-cards = list(map(int,sys.stdin.readline().split()))
-cards.sort()
+cards = set(map(int, sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
 nums = list(map(int,sys.stdin.readline().split()))
+
 for n in nums:
-    print(binSearch(cards,n), end=' ')
+    if n in cards:
+        print(1, end = " ")
+    else:
+        print(0, end = " ")
