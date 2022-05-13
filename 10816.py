0b4d227a4ad9c7e9142a51ca3ef23bf307b2ec3c
Ha0Lee <101060048+budKoder@users.noreply.github.com>
2022-05-13 15:33:11 +0900

    Refacor#01 use dictionary
    use dictionary to reduce time

--- 10816.py
+++ 10816.py
@@ -1,36 +1,20 @@
 import sys
-
-
-def getUpperBound(arr,n):
-    low = 0
-    high = len(arr)
-    while low < high:
-        mid = (low+high)//2
-        if arr[mid] <= n:
-            low = mid+1
-        else:
-            high = mid
-    return high
-
-
-def getLowerBound(arr,n):
-    low = 0
-    high = len(arr)
-    while low < high:
-        mid = (low+high)//2
-        if arr[mid]>=n:
-            high = mid
-        else:
-            low = mid+1
-    return high
-
-
 n = int(sys.stdin.readline())
 cards = list(map(int,sys.stdin.readline().split()))
-cards.sort()
+d = dict()
+for c in cards:
+    if c in d.keys():
+        d[c] += 1
+    else:
+        d[c] = 1
+
 m = int(sys.stdin.readline())
 li = list(map(int,sys.stdin.readline().split()))
-for l in li:
-    upper_bound = getUpperBound(cards,l)
-    lower_bound = getLowerBound(cards,l)
-    print(upper_bound - lower_bound, end=' ')
+result = []
+for x in li:
+    if x in d:
+        result.append(d[x])
+    else:
+        result.append(0)
+
+print(*result)
