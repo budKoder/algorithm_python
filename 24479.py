36236e76d9b495de7059cc33b73136cfbac1d9be
Ha0Lee <101060048+budKoder@users.noreply.github.com>
2022-05-20 16:48:45 +0900

    BOJ#24479 알고리즘 수업 - 깊이 우선 탐색 1

---
+++ 24479.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+
+def dfs(graph, visited, start):
+    global cnt
+    visited[start] = cnt
+    for i in graph[start]:
+        if visited[i] == 0:
+            cnt += 1
+            dfs(graph,visited,i)
+    return
+
+
+n,m,r = map(int,sys.stdin.readline().split())
+
+graph = [[]*n for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int,sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+for i in graph:
+    i.sort()
+
+visited = [0 for _ in range(n+1)]
+cnt = 1
+
+dfs(graph,visited,r)
+for i in range(1,n+1):
+    print(visited[i])
