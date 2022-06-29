e98b4d9a4eb2fd802f8dc82cc30ed78e0783c4c1
Ha0Lee <haleo1510@gmail.com>
2022-06-29 09:19:14 +0900

    PROG_lv1#49 콜라츠 추측

---
+++ lv1_49.py
@@ -0,0 +1,12 @@
+def solution(num):
+    answer = 0
+    while num > 1 and answer < 500:
+        if num%2 == 0:
+            num //= 2
+        else:
+            num = num*3 + 1
+        answer += 1
+    return -1 if answer >= 500 else answer
+
+n = 626331
+print(solution(n))
